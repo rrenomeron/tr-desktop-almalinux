3207fe4b205751176b67162fc68025d55fe9e5a6
Rich Renomeron <rrenomeron+github@gmail.com>
2026-03-18 12:58:07 -0400

    chore: remove extra pkgs in alma, add trust for rh container images

--- build/image-overrides.sh
+++ build/image-overrides.sh
@@ -13,4 +13,19 @@ for j in /etc/dconf/db/distro.d/08-tr-ui-fixes \
     sed -i 's/Adwaita Mono 13/Red Hat Mono 13/g' "$j"
 done
 
-dnf install -y just
+dnf install -y just 
+# Apparently this needs to be done for non-RHEL EL in order to download images from Red Hat.
+# See https://access.redhat.com/articles/3116561
+
+podman image trust set -f /etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release registry.access.redhat.com
+podman image trust set -f /etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release registry.redhat.io
+cat << EOF > /etc/containers/registries.d/registry.access.redhat.com.yaml
+docker:
+     registry.access.redhat.com:
+         sigstore: https://access.redhat.com/webassets/docker/content/sigstore
+EOF
+cat << EOF > /etc/containers/registries.d/registry.redhat.io.yaml
+docker:
+     registry.redhat.io:
+         sigstore: https://registry.redhat.io/containers/sigstore
+EOF
